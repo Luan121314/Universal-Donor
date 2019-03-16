3651cba0b2864b61ed1125130986f0bd05bea518
Luan <Luan.staner@gmail.com>
2019-03-16 16:52:47 -0300

    Criação de rota url para pagina login e cadastro

--- Universal_donor/urls.py
+++ Universal_donor/urls.py
@@ -3,5 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.res_request)
+    path('', views.res_request),
+    path('cadastro/',views.cadastro, name='cadastro'),
+    path('login/',views.login, name='login'),
+
+
 ]
--- Universal_donor/views.py
+++ Universal_donor/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def res_request(request):
     return render(request, 'index.html')
+
+def cadastro(request):
+    return render(request,'cadastro.html')
+def login(request):
+    return render(request,'login.html')
